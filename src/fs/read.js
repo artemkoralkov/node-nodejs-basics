2823126bed3ad038043a284527d46b52ade77e5e
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 14:26:06 +0300

    feat: implement read function from file sytem subtask

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,13 @@
+import { readFile } from 'fs/promises';
+
 export const read = async () => {
     // Write your code here 
+    readFile('src/fs/files/fileToRead.txt', 'utf-8')
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        throw new Error('FS operation failed');
+    });
 };
+read();
