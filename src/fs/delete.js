2160be204f65e467f2c548993a7844d4dc106c3e
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 14:04:24 +0300

    feat: implement remove function from file sytem subtask

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,11 @@
+import { unlink } from 'fs';
+
 export const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    unlink('src/fs/files/fileToRemove.txt', err => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    }); 
 };
+remove();
