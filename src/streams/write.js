74cf65f2bd065835712336d5f2a46dd0e4b9e711
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 13:05:37 +0300

    feat: implement write function from stream subtask

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,8 @@
+import { createWriteStream } from 'fs';
+
 export const write = async () => {
     // Write your code here 
+    //
+    process.stdin.pipe(createWriteStream('streams/files/fileToWrite.txt'));
 };
+write();
