d540822c90dc9bb2ba734961b434613dd7020b38
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 17:34:25 +0300

    refactor: add new line in transform output

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -10,7 +10,8 @@ export const transform = async () => {
                 .toString()
                 .split('')
                 .reverse()
-                .join('')
+                .join('') + '\n'
+                
             );
         }
     });
