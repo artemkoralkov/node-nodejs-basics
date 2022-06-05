19cda6cb596223fda8f0b20532fef02ed0741cb1
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 14:12:21 +0300

    feat: implement compress function from zlib subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,14 @@
+import { createReadStream, createWriteStream, unlink } from 'fs';
+import { createGzip } from 'zlib';
 export const compress = async () => {
     // Write your code here 
+    createReadStream('src/zip/files/fileToCompress.txt', 'utf-8')
+    .pipe(createGzip())
+    .pipe(createWriteStream('src/zip/files/archive.gz'))
+    .on('close', () => {
+        unlink('src/zip/files/fileToCompress.txt', (err) => {
+            console.log(err);
+        });
+    });
 };
+compress();
