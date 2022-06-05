65e4593d1e6afb2b6fdb59d90e99e83a6c7ba086
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 14:28:00 +0300

    feat: implement list function from file sytem subtask

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,13 @@
+import { readdir } from 'fs/promises';
+
 export const list = async () => {
     // Write your code here 
+    readdir('src/fs/files')
+    .then((files) => {
+        console.log(files);
+    })
+    .catch((err) => {
+        throw new Error('FS operation failed');
+    })
 };
+list();
