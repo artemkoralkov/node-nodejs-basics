06bf85ced036174d9b53a01a63b10ae4e01a7b66
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 12:55:23 +0300

    feat: implement create function from file system subtask

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,11 @@
+import { writeFile } from 'fs';
+
 export const create = async () => {
     // Write your code here 
+    writeFile('src/fs/files/fresh.txt', 'I am fresh and young', { flag: 'wx' }, (err) => {
+       if (err) {
+         throw new Error('FS operation failed');
+       }
+    })
 };
+create();
