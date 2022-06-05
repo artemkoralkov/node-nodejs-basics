1dd95dc18e68ce9fbbef3441e1899b6a089f5cd3
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-05 12:59:39 +0300

    feat: implement parseArgs function from command line interface subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2);
+    let res = '';
+    for(let i = 1; i < args.length; i += 2) {
+        res += `${args[i - 1].slice(2)} is ${args[i]}, `;
+    }
+    process.stdout.write(res.slice(0, res.length - 2));
 };
+parseArgs();
